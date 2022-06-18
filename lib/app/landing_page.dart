2e07076566e7ede91b7fd88cce0c9edeeb0ace36
Alsinus <alsinus.divergnight@gmail.com>
2022-06-18 19:39:05 +0200

    Accessing the Auth object via the AuthProvider

--- lib/app/landing_page.dart
+++ lib/app/landing_page.dart
@@ -2,27 +2,21 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:time_tracker_flutter_course/app/home/home_page.dart';
 import 'package:time_tracker_flutter_course/app/sign_in/sign_in_page.dart';
-import 'package:time_tracker_flutter_course/services/auth.dart';
+import 'package:time_tracker_flutter_course/services/auth_provider.dart';
 
 class LandingPage extends StatelessWidget {
-  const LandingPage({Key key, @required this.auth}) : super(key: key);
-  final AuthBase auth;
-
   @override
   Widget build(BuildContext context) {
+    final auth = AuthProvider.of(context);
     return StreamBuilder(
       stream: auth.authStateChanges(),
       builder: (context, snapchot) {
         if (snapchot.connectionState == ConnectionState.active) {
           final User user = snapchot.data;
           if (user == null) {
-            return SignInPage(
-              auth: auth,
-            );
+            return SignInPage();
           } else {
-            return HomePage(
-              auth: auth,
-            );
+            return HomePage();
           }
         }
         return Scaffold(
--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.indigo,
         ),
-        home: LandingPage(auth: Auth()),
+        home: LandingPage(),
       ),
     );
   }
