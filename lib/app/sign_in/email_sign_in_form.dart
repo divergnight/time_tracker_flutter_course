b1975f0485863bf459e4bbf6e35f57054f97a210
Alsinus <alsinus.divergnight@gmail.com>
2022-06-18 16:14:07 +0200

    Adding the submit buttons

--- lib/app/sign_in/email_sign_in_form.dart
+++ lib/app/sign_in/email_sign_in_form.dart
@@ -9,12 +9,43 @@ class EmailSignInForm extends StatelessWidget {
           hintText: 'test@test.com',
         ),
       ),
+      SizedBox(height: 8.0),
       TextField(
         decoration: InputDecoration(
           labelText: 'Password',
         ),
         obscureText: true,
-      )
+      ),
+      SizedBox(height: 8.0),
+      ElevatedButton(
+        child: Text('Sign In'),
+        style: ElevatedButton.styleFrom(
+          primary: Colors.grey[350],
+          onPrimary: Colors.black,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(2.0)),
+          ),
+        ),
+        onPressed: () {},
+      ),
+      SizedBox(height: 8.0),
+      TextButton(
+        onPressed: () {},
+        style: ButtonStyle(
+          overlayColor: MaterialStateProperty.all<Color>(
+            Colors.grey[300],
+          ),
+          padding: MaterialStateProperty.all<EdgeInsets>(
+            EdgeInsets.symmetric(horizontal: 16.0),
+          ),
+        ),
+        child: Text(
+          "Need an account ? Register",
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+      ),
     ];
   }
 
@@ -23,6 +54,7 @@ class EmailSignInForm extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(16.0),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         mainAxisSize: MainAxisSize.min,
         children: _buildChildren(),
       ),
