2894e31d37672d41e555569e2f2dcf6550cf0824
divergnight <alsinus.divergnight@gmail.com>
2022-06-20 09:09:13 +0200

    Reading data from Firestore

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -5,6 +5,7 @@ import 'package:time_tracker_flutter_course/services/api_path.dart';
 
 abstract class Database {
   Future<void> createJob(Job job);
+  void readJobs();
 }
 
 class FirestoreDatabase implements Database {
@@ -15,6 +16,15 @@ class FirestoreDatabase implements Database {
         path: APIPath.job(uid, 'job_abc'),
         data: job.toMap(),
       );
+
+  void readJobs() {
+    final path = APIPath.jobs(uid);
+    final reference = FirebaseFirestore.instance.collection(path);
+    final snapchots = reference.snapshots();
+    snapchots.listen((snapchot) {
+      snapchot.docs.forEach((snapchot) => print(snapchot.data()));
+    });
+  }
 
   Future<void> _setData({String path, Map<String, dynamic> data}) async {
     final reference = FirebaseFirestore.instance.doc(path);
