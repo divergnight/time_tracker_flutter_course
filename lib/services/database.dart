0123c6fd0400a755fe5ca2826d4db1675d4cc9ab
divergnight <alsinus.divergnight@gmail.com>
2022-06-20 08:42:50 +0200

    Adding a generic setData method

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -11,9 +11,13 @@ class FirestoreDatabase implements Database {
   FirestoreDatabase({@required this.uid}) : assert(uid != null);
   final String uid;
 
-  Future<void> createJob(Job job) async {
-    final path = APIPath.job(uid, 'job_abc');
-    final documentReference = FirebaseFirestore.instance.doc(path);
-    await documentReference.set(job.toMap());
+  Future<void> createJob(Job job) => _setData(
+        path: APIPath.job(uid, 'job_abc'),
+        data: job.toMap(),
+      );
+
+  Future<void> _setData({String path, Map<String, dynamic> data}) async {
+    final reference = FirebaseFirestore.instance.doc(path);
+    await reference.set(data);
   }
 }
