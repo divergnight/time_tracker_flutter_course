86cf45a12acbcb34f3ef068ed85931886d976c5f
divergnight <alsinus.divergnight@gmail.com>
2022-06-26 19:00:21 +0200

    Validating and saving Form data

--- lib/app/home/jobs/add_job_page.dart
+++ lib/app/home/jobs/add_job_page.dart
@@ -13,12 +13,52 @@ class AddJobPage extends StatefulWidget {
 }
 
 class _AddJobPageState extends State<AddJobPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  String _name;
+  int _ratePerHour;
+
+  bool _validateAndSaveForm() {
+    final form = _formKey.currentState;
+    if (form.validate()) {
+      form.save();
+      return true;
+    }
+    return false;
+  }
+
+  void _submit() {
+    if (_validateAndSaveForm()) {
+      print('form saved, name: $_name, ratePerHour: $_ratePerHour');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         elevation: 2.0,
         title: Text('New Job'),
+        actions: <Widget>[
+          TextButton(
+            onPressed: _submit,
+            style: ButtonStyle(
+              overlayColor: MaterialStateProperty.all<Color>(
+                Colors.indigo[600],
+              ),
+              padding: MaterialStateProperty.all<EdgeInsets>(
+                EdgeInsets.symmetric(horizontal: 16.0),
+              ),
+            ),
+            child: Text(
+              "Save",
+              style: TextStyle(
+                fontSize: 18.0,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
       ),
       body: _buildContents(),
       backgroundColor: Colors.grey[200],
@@ -41,6 +81,7 @@ class _AddJobPageState extends State<AddJobPage> {
 
   Widget _buildForm() {
     return Form(
+      key: _formKey,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: _buildFormChildren(),
@@ -52,11 +93,14 @@ class _AddJobPageState extends State<AddJobPage> {
     return [
       TextFormField(
         decoration: InputDecoration(labelText: 'Job name'),
+        validator: (value) => value.isNotEmpty ? null : "Name can't be empty",
+        onSaved: (value) => _name = value,
       ),
       TextFormField(
         decoration: InputDecoration(labelText: 'Rate per hour'),
         keyboardType:
             TextInputType.numberWithOptions(signed: false, decimal: false),
+        onSaved: (value) => _ratePerHour = int.tryParse(value) ?? 0,
       )
     ];
   }
