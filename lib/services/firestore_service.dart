71370e9070c2540b0c9ea875898090682b137484
divergnight <alsinus.divergnight@gmail.com>
2022-06-27 17:03:33 +0200

    Add optional filter to get entries in Firestore service

--- lib/services/firestore_service.dart
+++ lib/services/firestore_service.dart
@@ -23,13 +23,34 @@ class FirestoreService {
   Stream<List<T>> collectionStream<T>({
     @required String path,
     @required T Function(Map<String, dynamic> data, String documentId) builder,
+    Query Function(Query query) queryBuilder,
+    int Function(T lhs, T rhs) sort,
   }) {
-    final reference = FirebaseFirestore.instance.collection(path);
+    Query query = FirebaseFirestore.instance.collection(path);
+    if (queryBuilder != null) {
+      query = queryBuilder(query);
+    }
+    final snapshots = query.snapshots();
+    return snapshots.map((snapshot) {
+      final result = snapshot.docs
+          .map(
+            (snapshot) => builder(snapshot.data(), snapshot.id),
+          )
+          .where((value) => value != null)
+          .toList();
+      if (sort != null) {
+        result.sort(sort);
+      }
+      return result;
+    });
+  }
+
+  Stream<T> documentStream<T>({
+    @required String path,
+    @required T Function(Map<String, dynamic> data, String documentId) builder,
+  }) {
+    final reference = FirebaseFirestore.instance.doc(path);
     final snapshots = reference.snapshots();
-    return snapshots.map((snapshot) => snapshot.docs
-        .map(
-          (snapshot) => builder(snapshot.data(), snapshot.id),
-        )
-        .toList());
+    return snapshots.map((snapshot) => builder(snapshot.data(), snapshot.id));
   }
 }
