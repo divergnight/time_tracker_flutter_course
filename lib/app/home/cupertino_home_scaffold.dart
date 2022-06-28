0e2e94ba4eaac275034ed193c409c323092a0917
divergnight <alsinus.divergnight@gmail.com>
2022-06-28 09:54:33 +0200

    Creating a HomePage with a selected tab

---
+++ lib/app/home/cupertino_home_scaffold.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:time_tracker_flutter_course/app/home/tab_item.dart';
+
+class CupertinoHomeScaffold extends StatelessWidget {
+  const CupertinoHomeScaffold(
+      {@required this.currentTab, @required this.onSelectTab});
+
+  final TabItem currentTab;
+  final ValueChanged<TabItem> onSelectTab;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
