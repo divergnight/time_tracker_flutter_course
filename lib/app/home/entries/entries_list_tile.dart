f0fd39f76018a4904b9bd65839b62af586a3b7d3
divergnight <alsinus.divergnight@gmail.com>
2022-06-28 23:58:16 +0200

    Create EntriesListTile model class

---
+++ lib/app/home/entries/entries_list_tile.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class EntriesListTileModel {
+  const EntriesListTileModel({
+    @required this.leadingText,
+    @required this.trailingText,
+    this.middleText,
+    this.isHeader = false,
+  });
+  final String leadingText;
+  final String trailingText;
+  final String middleText;
+  final bool isHeader;
+}
