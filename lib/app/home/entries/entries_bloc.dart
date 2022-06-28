3a0c669bec8317455b28e8b0714c0ef25e218d6a
divergnight <alsinus.divergnight@gmail.com>
2022-06-28 23:59:08 +0200

    Complete EntriesBloc Provider

--- lib/app/home/entries/entries_bloc.dart
+++ lib/app/home/entries/entries_bloc.dart
@@ -1,6 +1,10 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:provider/provider.dart';
 import 'package:rxdart/rxdart.dart';
+import 'package:time_tracker_flutter_course/app/home/entries/daily_jobs_details.dart';
+import 'package:time_tracker_flutter_course/app/home/entries/entries_list_tile.dart';
 import 'package:time_tracker_flutter_course/app/home/entries/entry_job.dart';
+import 'package:time_tracker_flutter_course/app/home/job_entries/format.dart';
 import 'package:time_tracker_flutter_course/app/home/models/entry.dart';
 import 'package:time_tracker_flutter_course/app/home/models/job.dart';
 import 'package:time_tracker_flutter_course/services/database.dart';
@@ -26,5 +30,50 @@ class EntriesBloc {
       );
       return EntryJob(entry, job);
     }).toList();
+  }
+
+  /// Output stream
+  Stream<List<EntriesListTileModel>> get entriesTileModelStream =>
+      _allEntriesStream.map(((item) => _createModels(context, item)));
+
+  static List<EntriesListTileModel> _createModels(
+      BuildContext context, List<EntryJob> allEntries) {
+    if (allEntries.isEmpty) {
+      return [];
+    }
+    final allDailyJobsDetails = DailyJobsDetails.all(allEntries);
+
+    // total duration across all jobs
+    final totalDuration = allDailyJobsDetails
+        .map((dateJobsDuration) => dateJobsDuration.duration)
+        .reduce((value, element) => value + element);
+
+    // total pay across all jobs
+    final totalPay = allDailyJobsDetails
+        .map((dateJobsDuration) => dateJobsDuration.pay)
+        .reduce((value, element) => value + element);
+
+    Format format = Provider.of<Format>(context);
+    return <EntriesListTileModel>[
+      EntriesListTileModel(
+        leadingText: 'All Entries',
+        middleText: format.currency(totalPay),
+        trailingText: format.hours(totalDuration),
+      ),
+      for (DailyJobsDetails dailyJobsDetails in allDailyJobsDetails) ...[
+        EntriesListTileModel(
+          isHeader: true,
+          leadingText: format.date(dailyJobsDetails.date),
+          middleText: format.currency(dailyJobsDetails.pay),
+          trailingText: format.hours(dailyJobsDetails.duration),
+        ),
+        for (JobDetails jobDuration in dailyJobsDetails.jobsDetails)
+          EntriesListTileModel(
+            leadingText: jobDuration.name,
+            middleText: format.currency(jobDuration.pay),
+            trailingText: format.hours(jobDuration.durationInHours),
+          ),
+      ]
+    ];
   }
 }
