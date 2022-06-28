bbe2790be1ca1f3564bade6ba32dc3653542143e
divergnight <alsinus.divergnight@gmail.com>
2022-06-29 00:02:46 +0200

    Add EntriesBloc Provider inside entries page

--- lib/app/home/entries/entries_page.dart
+++ lib/app/home/entries/entries_page.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:time_tracker_flutter_course/app/home/entries/entries_bloc.dart';
+import 'package:time_tracker_flutter_course/app/home/entries/entries_list_tile.dart';
+import 'package:time_tracker_flutter_course/app/home/jobs/list_items_builder.dart';
+import 'package:time_tracker_flutter_course/services/database.dart';
 
 class EntriesPage extends StatelessWidget {
+  static Widget create(BuildContext context) {
+    final database = Provider.of<Database>(context, listen: false);
+    return Provider<EntriesBloc>(
+      create: (_) => EntriesBloc(context, database: database),
+      child: EntriesPage(),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Entries'),
-        centerTitle: true,
         elevation: 2.0,
       ),
       body: _buildContents(context),
@@ -14,6 +26,16 @@ class EntriesPage extends StatelessWidget {
   }
 
   Widget _buildContents(BuildContext context) {
-    return Container();
+    final bloc = Provider.of<EntriesBloc>(context, listen: false);
+    return StreamBuilder<List<EntriesListTileModel>>(
+      stream: bloc.entriesTileModelStream,
+      builder: (context, snapshot) {
+        return ListItemsBuilder<EntriesListTileModel>(
+          snapshot: snapshot,
+          itemBuilder: (context, model) =>
+              Text(model.leadingText, style: TextStyle(fontSize: 16.0)),
+        );
+      },
+    );
   }
 }
--- lib/app/home/home_page.dart
+++ lib/app/home/home_page.dart
@@ -22,7 +22,7 @@ class _HomePageState extends State<HomePage> {
   Map<TabItem, WidgetBuilder> get widgetBuilders {
     return {
       TabItem.jobs: (_) => JobsPage(),
-      TabItem.entries: (_) => EntriesPage(),
+      TabItem.entries: (context) => EntriesPage.create(context),
       TabItem.account: (_) => AccountPage(),
     };
   }
