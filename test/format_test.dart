1ac506223a5aeb46e67b3ded5eff2a54c4005243
divergnight <alsinus.divergnight@gmail.com>
2022-06-30 18:37:22 +0200

    The setUp method and testing date formatting

--- test/format_test.dart
+++ test/format_test.dart
@@ -1,4 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:intl/intl.dart';
 import 'package:time_tracker_flutter_course/app/home/job_entries/format.dart';
 
 void main() {
@@ -14,6 +16,19 @@ void main() {
     });
     test('decimal', () {
       expect(Format.singleton.hours(4.5), '4.5h');
+    });
+  });
+
+  group('date - US Locale', () {
+    setUp(() async {
+      Intl.defaultLocale = 'en_US';
+      await initializeDateFormatting(Intl.defaultLocale);
+    });
+    test('2019-08-12', () {
+      expect(Format.singleton.date(DateTime(2019, 08, 12)), 'Aug 12, 2019');
+    });
+    test('2019-08-16', () {
+      expect(Format.singleton.date(DateTime(2019, 08, 16)), 'Aug 16, 2019');
     });
   });
 }
