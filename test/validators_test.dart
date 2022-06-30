c2368d8cd97d78d20b3523ed8a6c365bdffbde6a
divergnight <alsinus.divergnight@gmail.com>
2022-06-30 17:11:38 +0200

    Checking and fixing errors in tests

--- test/validators_test.dart
+++ test/validators_test.dart
@@ -5,6 +5,10 @@ void main() {
   test('non empty string', () {
     final validator = NonEmptyStringValidator();
     expect(validator.isValid('test'), true);
+  });
+
+  test('empty string', () {
+    final validator = NonEmptyStringValidator();
     expect(validator.isValid(''), false);
   });
 }
