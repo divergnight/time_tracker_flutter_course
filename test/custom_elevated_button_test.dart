43293debfbb27e81104ea8f552c5ff966532b076
divergnight <alsinus.divergnight@gmail.com>
2022-07-01 08:27:43 +0200

    Introduction to widget tests

---
+++ test/custom_elevated_button_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:time_tracker_flutter_course/common_widgets/custom_elevated_button.dart';
+
+void main() {
+  testWidgets('', (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(home: CustomElevatedButton()));
+  });
+}
